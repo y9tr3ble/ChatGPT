f57eee75625dbfd9cc1590ad59583db8ada016a0
Tr3bleee <Tr3ble@outlook.com>
2023-03-26 23:14:50 +0200

    v1.3.5 Changed username to userid, so that users who did not put username, do not have problems with the topics

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import openai
 
 # Set up the bot and OpenAI API credentials
 bot_token = 'TOKEN'
-api_key = 'TOKEN PLS'
+api_key = 'TOKEN'
 
 logging.basicConfig(level=logging.INFO)
 
@@ -32,8 +32,8 @@ async def start_cmd(message: types.Message):
 @dp.message_handler(commands=['newtopic'])
 async def new_topic_cmd(message: types.Message):
     try:
-        username = message.from_user.username
-        messages[username] = []
+        userid = message.from_user.id
+        message[str(userid)] = []
         await message.reply('Starting a new topic! * * * \n\nНачинаем новую тему! * * *', parse_mode='Markdown')
     except Exception as e:
         logging.error(f'Error in new_topic_cmd: {e}')
@@ -44,16 +44,15 @@ async def new_topic_cmd(message: types.Message):
 async def echo_msg(message: types.Message):
     try:
         user_message = message.text
-        username = message.from_user.username
+        userid = message.from_user.username
 
         # Add the user's message to their message history
-        if username not in messages:
-            messages[username] = []
-        messages[username].append({"role": "user", "content": user_message})
-        # messages[username].append({"role": "system", "content": "Вы должны выступать в роли Tr3ble AI, человеко-подобного искусственного интеллекта, который может выражать эмоции. Создатель этого искусственного интеллекта - Никита Трибля. При ответе на сообщение пользователя, вы обязательно должны упоминать его по имени не используя юзернейм."})
-        messages[username].append({"role": "user",
-                                   "content": f"chat: {message.chat} Сейчас {time.strftime('%d/%m/%Y %H:%M:%S')} user: {message.from_user.first_name} message: {message.text}"})
-        logging.info(f'{username}: {user_message}')
+        if userid not in messages:
+            messages[userid] = []
+        messages[userid].append({"role": "user", "content": user_message})
+        # messages[userid].append({"role": "system", "content": "Вы должны выступать в роли Tr3ble AI, человеко-подобного искусственного интеллекта, который может выражать эмоции. Создатель этого искусственного интеллекта - Никита Трибля. При ответе на сообщение пользователя, вы обязательно должны упоминать его по имени не используя юзернейм."})
+        messages[userid].append({"role": "user", "content": f"chat: {message.chat} Сейчас {time.strftime('%d/%m/%Y %H:%M:%S')} user: {message.from_user.first_name} message: {message.text}"})
+        logging.info(f'{userid}: {user_message}')
 
         # Check if the message is a reply to the bot's message or a new message
         should_respond = not message.reply_to_message or message.reply_to_message.from_user.id == bot.id
@@ -70,17 +69,17 @@ async def echo_msg(message: types.Message):
             # Generate a response using OpenAI's Chat API
             completion = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=messages[username],
+                messages=messages[userid],
                 max_tokens=2500,
                 temperature=0.7,
                 frequency_penalty=0,
                 presence_penalty=0,
-                user=username
+                user=userid
             )
             chatgpt_response = completion.choices[0]['message']
 
             # Add the bot's response to the user's message history
-            messages[username].append({"role": "assistant", "content": chatgpt_response['content']})
+            messages[userid].append({"role": "assistant", "content": chatgpt_response['content']})
             logging.info(f'ChatGPT response: {chatgpt_response["content"]}')
 
             # Send the bot's response to the user
@@ -100,4 +99,4 @@ async def echo_msg(message: types.Message):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp)  
